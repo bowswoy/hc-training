f47889909ff83850d3e1eaa6468d2be06099b2fb
bowswoy <puwanai.sangphon@gmail.com>
2019-09-09 13:35:43 +0700

    new

---
+++ chart-php-1.php
@@ -0,0 +1,75 @@
+<?php
+// echo rand(10,100);
+
+$data = array(
+    array('ส่วนกลาง', rand(10,100)),
+    array('ภาคกลาง', rand(10,100)),
+    array('ภาคเหนือ', rand(10,100)),
+    array('ภาคอีสาน', rand(10,100)),
+    array('ภาคใต้', rand(10,100)),
+    array('ชายแดนใต้', rand(10,100)),
+);
+
+// echo '<pre>';
+// // print_r($data);
+// echo json_encode($data);
+// echo '</pre>';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div id="container" style="width:100%; height:400px;"></div>
+
+    <script src="highcharts/highcharts.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            Highcharts.chart('container', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'สถิติข้อมูลการให้บริการประชาชน รายภูมิภาค'
+                },
+                subtitle: {
+                    text: 'ข้อมูล ณ วันที่ <?php echo date("d/m/Y"); ?>'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.y} ราย</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.2f} %'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'จำนวน',
+                    colorByPoint: true,
+                    data: <?php echo json_encode($data); ?>
+                }]
+            });
+        });
+    </script>
+</body>
+
+</html>
